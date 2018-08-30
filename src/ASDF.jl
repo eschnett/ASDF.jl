93efd7ddbdbd2146510112274f23ff948923adaf
Erik Schnetter <schnetter@gmail.com>
2018-08-29 22:40:48 -0400

    Allow failures on nightly

--- src/ASDF.jl
+++ src/ASDF.jl
@@ -1,17 +1,15 @@
 module ASDF
 
-export asdf
-
 using PyCall
 
+export asdf
 const asdf = PyNULL()
-
 function __init__()
     copy!(asdf, pyimport_conda("asdf", "asdf", "astropy"))
 end
 
 function Base.VersionNumber(v::PyObject)
-    VersionNumber(v[:major], v[:minor], v[:patch])
+    VersionNumber(v[:major], v[:minor], v[:patch], v[:prerelease], v[:build])
 end
 
 end
