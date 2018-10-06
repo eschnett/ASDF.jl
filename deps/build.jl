4a7233b58ce989ef5aecd0327cf68e56945d3cdb
Erik Schnetter <schnetter@gmail.com>
2018-10-06 11:36:22 -0400

    Always conda-add asdf module twice

--- deps/build.jl
+++ deps/build.jl
@@ -1,24 +1,24 @@
-# using Conda
-# using PyCall
-#
-# if PyCall.conda
-#     # Check whether asdf is already installed
-#     try
-#         pyimport("asdf")
-#     catch
-#         # Install asdf
-#         Conda.add_channel("conda-forge")
-#
-#         # The first install sometimes fails with a strange numpy
-#         # problem. We thus run it as throw-away command. Note that
-#         # calling `Conda.add` aborts on errors, so we have to use this
-#         # lower-level equivalent.
-#         try
-#             Conda.runconda(`install -y asdf`)
-#         catch
-#         end
-#     end
-# end
+using Conda
+using PyCall
+
+if PyCall.conda
+    # Check whether asdf is already installed
+    try
+        pyimport("asdf")
+    catch
+        # Install asdf
+        Conda.add_channel("conda-forge")
+
+        # The first install sometimes fails with a strange numpy
+        # problem. We thus run it as throw-away command. Note that
+        # calling `Conda.add` aborts on errors, so we have to use this
+        # lower-level equivalent.
+        try
+            Conda.runconda(`install -y asdf`)
+        catch
+        end
+    end
+end
 
 
 
@@ -33,19 +33,3 @@ if version < v"2.0.3"
     @error "This version is unsupported"
     exit(1)
 end
-
-
-
-# using PyCall
-#
-# try
-#     pyimport("pip")
-# catch
-#     get_pip = joinpath(dirname(@__FILE__), "get-pip.py")
-#     download("https://bootstrap.pypa.io/get-pip.py", get_pip)
-#     run(`$(PyCall.python) $get_pip --user`)
-#     run(`$(PyCall.python) -m pip install --upgrade pip`)
-# end
-#
-# run(`$(PyCall.python) -m pip install --user astropy_helpers`)
-# run(`$(PyCall.python) -m pip install --user git+https://github.com/spacetelescope/asdf.git@6705cd6fd1b040ded28d8c58b9aa62be87f5584e`)
