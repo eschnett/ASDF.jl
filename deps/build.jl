ba17cb48039576f7e3ccb9a16cda3f2c52d7bdde
Erik Schnetter <schnetter@gmail.com>
2018-10-06 11:26:42 -0400

    Clean up white space

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,6 @@
 # using Conda
 # using PyCall
-# 
+#
 # if PyCall.conda
 #     # Check whether asdf is already installed
 #     try
@@ -8,7 +8,7 @@
 #     catch
 #         # Install asdf
 #         Conda.add_channel("conda-forge")
-#     
+#
 #         # The first install sometimes fails with a strange numpy
 #         # problem. We thus run it as throw-away command. Note that
 #         # calling `Conda.add` aborts on errors, so we have to use this
@@ -22,7 +22,7 @@
 
 
 
- using PyCall
+using PyCall
 
 const asdf = pyimport_conda("asdf", "asdf", "conda-forge")
 # const asdf = pyimport_conda("asdf", "asdf", "astropy")
@@ -37,7 +37,7 @@ end
 
 
 # using PyCall
-# 
+#
 # try
 #     pyimport("pip")
 # catch
@@ -46,6 +46,6 @@ end
 #     run(`$(PyCall.python) $get_pip --user`)
 #     run(`$(PyCall.python) -m pip install --upgrade pip`)
 # end
-# 
+#
 # run(`$(PyCall.python) -m pip install --user astropy_helpers`)
 # run(`$(PyCall.python) -m pip install --user git+https://github.com/spacetelescope/asdf.git@6705cd6fd1b040ded28d8c58b9aa62be87f5584e`)
