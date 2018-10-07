e1d0bf0f97e892860a69b1bcc871bedfc769e9ca
Erik Schnetter <schnetter@gmail.com>
2018-10-07 10:34:29 -0400

    Use astropy Conda channel

--- deps/build.jl
+++ deps/build.jl
@@ -1,31 +1,31 @@
-using Conda
+# using Conda
+# using PyCall
+#
+# if PyCall.conda
+#     # Check whether asdf is already installed
+#     try
+#         pyimport("asdf")
+#     catch
+#         # Install asdf
+#         Conda.add_channel("conda-forge")
+#
+#         # The first install sometimes fails with a strange numpy
+#         # problem. We thus run it as throw-away command. Note that
+#         # calling `Conda.add` aborts on errors, so we have to use this
+#         # lower-level equivalent.
+#         try
+#             Conda.runconda(`install -y asdf`)
+#         catch
+#         end
+#     end
+# end
+
+
+
 using PyCall
 
-if PyCall.conda
-    # Check whether asdf is already installed
-    try
-        pyimport("asdf")
-    catch
-        # Install asdf
-        Conda.add_channel("conda-forge")
-
-        # The first install sometimes fails with a strange numpy
-        # problem. We thus run it as throw-away command. Note that
-        # calling `Conda.add` aborts on errors, so we have to use this
-        # lower-level equivalent.
-        try
-            Conda.runconda(`install -y asdf`)
-        catch
-        end
-    end
-end
-
-
-
-using PyCall
-
-const asdf = pyimport_conda("asdf", "asdf", "conda-forge")
-# const asdf = pyimport_conda("asdf", "asdf", "astropy")
+# const asdf = pyimport_conda("asdf", "asdf", "conda-forge")
+const asdf = pyimport_conda("asdf", "asdf", "astropy")
 
 version = VersionNumber(asdf[:__version__])
 @info "Using Python asdf library version $version"
